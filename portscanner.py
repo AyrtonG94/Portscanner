a92ba73abf23bb356abbb80fb31a463c5871cb69
AyrtonG94 <ayrton.gomes@live.com>
2026-02-08 20:13:27 -0300

    Function: TCP SCAN

--- portscanner.py
+++ portscanner.py
@@ -9,17 +9,23 @@ args = parser.parse_args()
 ip = args.target
 list_ports = args.ports
 ports = list_ports.split(',')
-
+tcp_args = args.tcp
+ 
 def scanTCP():
-
- with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socketCliente: 
+ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socketClient: 
   for i in ports:
      port = int(i)
-     socketCliente.settimeout(0.5)
-     result = socketCliente.connect_ex((ip,port))
+     socketClient.settimeout(0.5)
+     result = socketClient.connect_ex((ip,port))
      if result == 0:
-          print('Porta aberta')
+       print('Porta aberta')
      else:
-          print('Porta fechada ou filtrada')
-          
-     socketCliente.close()
+      print('Porta fechada ou filtrada')
+     socketClient.close()
+
+
+if tcp_args:
+    scanTCP()
+
+else:
+   print('teste')
